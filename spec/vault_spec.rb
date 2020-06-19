0ca3b562657eebad5cd236242b2320c9395cce2d
William Akin <wakin66@gmail.com>
2020-06-19 14:04:24 -0400

    Initial Pending specs for Vault class

--- spec/vault_spec.rb
+++ spec/vault_spec.rb
@@ -1,5 +1,42 @@
 require 'vault'
 
 describe Vault do
+    subject(:vault) {Vault.new}
+
+    describe "#initialize" do
+        it "sets @rocks as an empty Array"
+
+        it "sets @rooms as an empty Array"
+
+        it "sets @storage as an empty Hash"
+
+        it "sets @inventory as an empty Array"
+
+        it "sets @emergency_data as an empty Hash"
+
+        it "sets @room_consumption as an empty Hash"
+
+        it "sets @dweller_water_consumption as an empty Hash"
+
+        it "sets @dweller_food_consumption as an empty Hash"
+
+        it "sets @lunchbox_Random_generator as nil" #Made into a symbol later
+
+        it "sets @lunch_boxes_by_type as an empty Array"
+
+        it "sets @lunch_boxes_count to 0" #possibly change this to a function to dynamically change
+
+        it "sets @vault_name to as a String"
+
+        it "sets @vault_mode as nil" #Made into a symbol later
+
+        it "sets @vault_theme to 0"
+
+        it "sets @achievements as an empty Hash"
+
+        it "sets @wasteland as an empty Hash"
+
+        it "sets @dwellers as an empty Array"
+    end
 
 end

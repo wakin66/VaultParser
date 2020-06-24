35ba3776d29dd82bdac690e16eb0c00e8f2fa9ab
William Akin <wakin66@gmail.com>
2020-06-23 22:18:41 -0400

    Added basic structure to files so rspec could run

---
+++ spec/dweller_spec.rb
@@ -0,0 +1,26 @@
+require 'dweller'
+
+describe Dweller do
+
+    describe "#initialize" do
+        it "takes a Hash as an argument"
+
+        it "sets @serializeID to an integer"
+
+        it "sets @first_name to a String"
+
+        it "sets @last_name to a String"
+
+        it "sets @happiness to an Integer"
+
+        it "sets @health_value to an Integer"
+
+        it "sets @radiation_value to an Integer"
+
+        it "sets @max_health to an Integer"
+
+        it "sets @experience_value to an Integer"
+
+        it "sets @current_level to an Integer"
+    end
+end
